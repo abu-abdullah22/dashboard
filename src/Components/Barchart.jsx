e39e64e42b53e417c529963f42053c671e341467
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-06-30 20:53:11 +0600

    tried to make the barchart more correct

--- src/Components/Barchart.jsx
+++ src/Components/Barchart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ArrowUp, IconlyProvider } from 'react-iconly';
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
     { date: '01', lastWeek: 3000, last6Days: 2000 },
@@ -21,27 +21,32 @@ const data = [
 
 const Barchart = () => {
     return (
-      <IconlyProvider>
-          <div className='ml-10 mt-10 space-y-2'>
-                <h2 className='text-[#000000]'>Revenue</h2>
+        <IconlyProvider>
+            <div className='ml-10 mt-10  overflow-x-auto'>
+               <div className='space-y-2'>
+               <h2 className='text-[#000000]'>Revenue</h2>
                 <p className='text-[#000000] text-xl font-medium'>IDR 7.852.000</p>
-                <p className='flex items-center gap-1'><span className='text-[#149D52] flex items-center'><ArrowUp />2.1%</span> vs last week</p>
-               
-      <BarChart
-        width={678}
-        height={300}
-        data={data} barCategoryGap={15} barGap={8}
-        margin={{ top: 0, right: 0, left:0, bottom: 0 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="last6Days" fill="#5A6ACF" name="Last 6 days"  />
-        <Bar dataKey="lastWeek" fill="#E6E8EC" name="Last Week" />
-      </BarChart>
+                <p className='flex items-center gap-1 '><span className='text-[#149D52] flex items-center '><ArrowUp />2.1%</span> vs last week</p>
+               </div>
+
+                <p className='text-[#000000] text-[17px] mt-6
+                 opacity-50'>Sales from 1-12 Dec, 2020</p>
+
+                <BarChart
+                    width={678}
+                    height={250}
+                    data={data} barCategoryGap={15} barGap={8}
+                    margin={{ top: -10, right: 0, left: 0, bottom: 0 }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" vertical={false}/>
+                    <XAxis dataKey="date" axisLine={{stroke: '#737B8B'}} tickLine={false}  stroke='#737B8B'  />
+                    <Tooltip />
+                    <Legend align='left' iconType='round' />
+                    <Bar dataKey="last6Days" fill="#5A6ACF" name="Last 6 days" />
+                    <Bar dataKey="lastWeek" fill="#E6E8EC" name="Last Week" />
+                </BarChart>
             </div>
-      </IconlyProvider>
+        </IconlyProvider>
     );
 };
 
